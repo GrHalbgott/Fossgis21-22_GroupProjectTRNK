1a7aee1c8cdb1d5ff2c0e367b2cd1d8c2520242f
Niko Kolaxidis <92852053+GrHalbgott@users.noreply.github.com>
2022-01-19 23:47:53 +0100

    Update ndvi.bat

--- Scripts/ndvi.bat
+++ Scripts/ndvi.bat
@@ -1,1 +1,1 @@
-gdal_calc.py -A ..\data\Sentinel_2\*B08.tif -B ..\data\Sentinel_2\*B04.tif --outfile=..\data\ndvi.tif --calc="(A - B)/(A + B)"
+gdal_calc.py -A ..\data\Sentinel_2\*B08.tif* -B ..\data\Sentinel_2\*B04.tif* --outfile=..\data\ndvi.tif --calc="(A - B)/(A + B)"
