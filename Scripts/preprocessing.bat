3e272b284b5d61346adb4b88092c21c44e6fa562
Niko Kolaxidis <92852053+GrHalbgott@users.noreply.github.com>
2022-01-26 22:06:22 +0100

    Delete preprocessing.bat

--- Scripts/preprocessing.bat
+++
@@ -1,7 +0,0 @@
-set roi=Heidelberg
-
-ogr2ogr -f "ESRI Shapefile" ./data/%roi%.shp -lco ENCODING=UTF-8 -t_srs EPSG:25832 -sql "SELECT * FROM gadm36_DEU_3 WHERE NAME_3='%roi%'" ./data/gadm36_DEU.gpkg
-
-gdalwarp -cutline ./data/%roi%.shp -crop_to_cutline -t_srs EPSG:25832 -dstnodata -9999 ./data/B04.jp2 ./data/%roi%_RED.tif
-
-gdalwarp -cutline ./data/%roi%.shp -crop_to_cutline -t_srs EPSG:25832 -dstnodata -9999 ./data/B08.jp2 ./data/%roi%_NIR.tif
